'data' is now a kwarg rather than arbitrary inputs. Fixed bug in broadcasting of singleton parent values

dms_stan/model/dms_stan_model.py
@@ -208,7 +208,7 @@ class Model(ABC):
         """
 
         def get_dimnames_squeezed_draw(
-            draw: npt.NDArray,
+            draw: Union[npt.NDArray, Constant],
         ) -> tuple[tuple[str, ...], npt.NDArray]:
             """
             For a draw, gets the dimension names and squeezes to remove all singleton
@@ -217,6 +217,13 @@ class Model(ABC):
             # Set up variables
             singleton_axes: list[str] = []
             dimnames: list[str] = []
+
+            # If the draw is a constant, then we use its value as the draw. We also
+            # repeat the draw to match the number of samples.
+            if isinstance(draw, Constant):
+                draw = draw.value
+                if draw.shape[0] == 1:
+                    draw = np.broadcast_to(draw, (n,) + draw.shape[1:])
 
             # Populate the singleton axes and dimnames based on the shape of the
             # draw
@@ -265,7 +272,7 @@ class Model(ABC):
                     for component, draw in draws.items()
                 },
                 coords={
-                    parent.model_varname: get_dimnames_squeezed_draw(parent.value)
+                    parent.model_varname: get_dimnames_squeezed_draw(parent)
                     for component in self.all_model_components
                     for parent in component.parents
                     if isinstance(component, TransformedParameter)
@@ -297,7 +304,7 @@ class Model(ABC):
         epochs: int = DEFAULT_N_EPOCHS,
         early_stop: int = DEFAULT_EARLY_STOP,
         lr: float = DEFAULT_LR,
-        **observed_data: Union[torch.Tensor, npt.NDArray],
+        data: Optional[dict[str, Union[torch.Tensor, npt.NDArray]]] = None,
     ) -> MAP:
         """
         Approximate the maximum a posteriori (MAP) estimate of the model parameters.
@@ -305,15 +312,18 @@ class Model(ABC):
         sum of `log_pdf` and `log_pmf` for all distributions. The parameter values
         that minimize this loss are then returned.
         """
+        # Set the default value for observed data
+        data = data or {}
+
         # Observed data to tensors
-        observed_data = {
+        data = {
             k: torch.tensor(v)
-            for k, v in observed_data.items()
+            for k, v in data.items()
             if not isinstance(v, torch.Tensor)
         }
 
         # Check observed data
-        check_observable_data(self, observed_data)
+        check_observable_data(self, data)
 
         # Fit the model
         pytorch_model = self.to_pytorch()
@@ -321,7 +331,7 @@ class Model(ABC):
             epochs=epochs,
             early_stop=early_stop,
             lr=lr,
-            **observed_data,
+            data=data,
         )
 
         # Get the MAP estimate for all model parameters
@@ -375,11 +385,15 @@ class Model(ABC):
         cpp_options: Optional[dict[str, Any]] = DEFAULT_CPP_OPTIONS,
         user_header: Optional[str] = DEFAULT_USER_HEADER,
         inits: Optional[str] = "prior",
+        data: Optional[dict[str, npt.NDArray]] = None,
         **sample_kwargs,
     ) -> CmdStanMCMC:
         """Samples from the model using MCMC. This is a wrapper around the `sample`
         method of the `StanModel` class.
         """
+        # Get the default observed data
+        data = data or {}
+
         # Build the Stan model
         stan_model = self.to_stan(
             output_dir=output_dir,
@@ -393,7 +407,7 @@ class Model(ABC):
         sample_kwargs["output_dir"] = stan_model.output_dir
 
         # Sample from the model
-        samples = stan_model.sample(inits=inits, **sample_kwargs)
+        samples = stan_model.sample(inits=inits, data=data, **sample_kwargs)
 
         # Run diagnostics
         print("Running diagnostics...")
@@ -401,7 +415,7 @@ class Model(ABC):
 
         return samples
 
-    def prior_predictive(self, *, copy_model: bool = False) -> pn.WidgetBox:
+    def prior_predictive(self, *, copy_model: bool = False) -> pn.Row:
         """
         Creates an interactive plot of the prior predictive distribution of the
         model. The plot can be used to update the model's parameters dynamically.

dms_stan/model/pytorch.py
@@ -17,14 +17,12 @@ from dms_stan.defaults import DEFAULT_EARLY_STOP, DEFAULT_LR, DEFAULT_N_EPOCHS
 from dms_stan.model.components import Multinomial
 
 
-def check_observable_data(
-    model: "dms_model.Model", observed_data: dict[str, torch.Tensor]
-):
+def check_observable_data(model: "dms_model.Model", data: dict[str, torch.Tensor]):
     """Makes sure that the correct observables are provided for a givne model."""
     # There must be perfect overlap between the keys of the provided data and the
     # expected observations
     expected_set = set(model.observable_dict.keys())
-    provided_set = set(observed_data.keys())
+    provided_set = set(data.keys())
     missing = expected_set - provided_set
     extra = provided_set - expected_set
 
@@ -43,11 +41,11 @@ def check_observable_data(
 
     # Shapes must match
     for name, param in model.observable_dict.items():
-        if observed_data[name].shape != param.shape:
+        if data[name].shape != param.shape:
             raise ValueError(
                 f"The shape of the provided data for observable {name} does not match "
                 f"the expected shape. Expected: {param.shape}, provided: "
-                f"{observed_data[name].shape}"
+                f"{data[name].shape}"
             )
 
 
@@ -80,7 +78,7 @@ class PyTorchModel(nn.Module):
         # Record learnable parameters such that they can be recognized by PyTorch
         self.learnable_params = nn.ParameterList(learnable_params)
 
-    def forward(self, **observed_data: torch.Tensor) -> torch.Tensor:
+    def forward(self, **data: torch.Tensor) -> torch.Tensor:
         """
         Each observation is passed in as a keyword argument whose name matches the
         name of the corresponding observable distribution in the `dms_stan.model.Model`
@@ -94,7 +92,7 @@ class PyTorchModel(nn.Module):
             self.model.parameter_dict.items(), self.model.observable_dict.items()
         ):
             # Calculate the log probability of the observed data given the parameters
-            temp_log_prob = param.get_torch_logprob(observed=observed_data.get(name))
+            temp_log_prob = param.get_torch_logprob(observed=data.get(name))
 
             # Log probability should be 0-dimensional if anything but a Multinomial
             assert temp_log_prob.ndim == 0 or isinstance(param, Multinomial)
@@ -106,20 +104,21 @@ class PyTorchModel(nn.Module):
 
     def fit(
         self,
+        *,
         epochs: int = DEFAULT_N_EPOCHS,
         early_stop: int = DEFAULT_EARLY_STOP,
         lr: float = DEFAULT_LR,
-        **observed_data: Union[torch.Tensor, npt.NDArray, float, int],
+        data: dict[str, Union[torch.Tensor, npt.NDArray, float, int]],
     ) -> torch.Tensor:
         """Optimizes the parameters of the model."""
         # Any observed data that is not a tensor is converted to a tensor
-        observed_data = {
+        data = {
             k: torch.tensor(v) if not isinstance(v, torch.Tensor) else v
-            for k, v in observed_data.items()
+            for k, v in data.items()
         }
 
         # Check the observed data
-        check_observable_data(self.model, observed_data)
+        check_observable_data(self.model, data)
 
         # Train mode. This should be a null-op.
         self.train()
@@ -137,7 +136,7 @@ class PyTorchModel(nn.Module):
             for epoch in range(epochs):
 
                 # Get the loss
-                log_loss = -1 * self(**observed_data)
+                log_loss = -1 * self(**data)
 
                 # Step the optimizer
                 optim.zero_grad()
@@ -173,7 +172,7 @@ class PyTorchModel(nn.Module):
 
         # Get a final loss
         with torch.no_grad():
-            loss_trajectory[epoch + 1] = -1 * self(**observed_data).item()
+            loss_trajectory[epoch + 1] = -1 * self(**data).item()
 
         # Trim off the None values of the loss trajectory and convert to a tensor
         return torch.tensor(loss_trajectory[: epoch + 2], dtype=torch.float32)