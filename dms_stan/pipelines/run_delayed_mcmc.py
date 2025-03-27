5264042f05445c4beaaa8163418a43a6e9a02f26
Bruce Wittmann <bwittmann@microsoft.com>
2025-03-27 14:06:14 -0700

    Added option to run sampling delayed from a script

---
+++ dms_stan/pipelines/run_delayed_mcmc.py
@@ -0,0 +1,26 @@
+"""Runs the `Model.mcmc` method using a delayed object file."""
+
+import argparse
+
+from dms_stan.model import run_delayed_mcmc
+
+
+def main():
+    """Runs the script."""
+    # Build the argument parser
+    parser = argparse.ArgumentParser(
+        description="Runs the `Model.mcmc` method using a delayed object file."
+    )
+    parser.add_argument(
+        "filepath",
+        type=str,
+        help="Path to the delayed object file.",
+    )
+
+    # Parse the arguments and run the script
+    args = parser.parse_args()
+    run_delayed_mcmc(args.filepath)
+
+
+if __name__ == "__main__":
+    main()
