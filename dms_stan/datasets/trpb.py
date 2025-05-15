4aae420c98310cdb90e1d84c0146272c7cef4c50
Bruce Wittmann <bwittmann@microsoft.com>
2025-05-14 17:37:46 -0700

    Added new distributions

--- dms_stan/datasets/trpb.py
+++ dms_stan/datasets/trpb.py
@@ -53,7 +53,7 @@ def load_trpb_dataset(filepath: str) -> dict[str, npt.NDArray | list[str]]:
     }
 
 
-class TrpBBaseGrowthModel(dms.Model):
+class BaseGrowthModel(dms.Model):
     """Base class for all TrpB growth models."""
 
     def __init__(
@@ -62,8 +62,7 @@ class TrpBBaseGrowthModel(dms.Model):
         starting_counts: npt.NDArray[np.integer],
         timepoint_counts: npt.NDArray[np.integer],
         alpha: float = 0.75,
-        inv_r_alpha: float = 2.5,
-        inv_r_beta: float = 0.5,
+        **hyperparameters,
     ):
 
         # Set the default data
@@ -116,27 +115,13 @@ class TrpBBaseGrowthModel(dms.Model):
         # Also define time as a constant
         self.tg0 = dms_components.Constant(times[None, 1:, None], togglable=False)
 
-        # Every variant has a fundamental "rate" which will be the same across
-        # experiments. This rate is modeled by the Gamma distribution and is the
-        # inverse of the growth rate (so that we can use it as the exponential rate
-        # parameter).
-        self.inv_r_mean = dms_components.Gamma(
-            alpha=inv_r_alpha,
-            beta=inv_r_beta,
-            shape=(self.n_variants,),
-        )
-
-        # The inverse rate is the beta parameter for the exponential distributions
-        # describing the growth rates in each experiment.
-        self.r = dms_components.Exponential(
-            beta=self.inv_r_mean,
-            shape=(self.n_replicates, 1, self.n_variants),
-        )
-
         # Every model has a theta_t0 parameter, which gives the starting proportions
         # at the beginning of the experiment. We assume that its possible values
         # are a fundamental property of the variant; hence, we define one per variant.
         self.theta_t0 = dms_components.Dirichlet(alpha=alpha, shape=(self.n_variants,))
+
+        # Define non-base parameters
+        self._define_non_base_parameters(**hyperparameters)
 
         # Define the growth function
         self.raw_abundances = self._define_growth_function()
@@ -153,6 +138,13 @@ class TrpBBaseGrowthModel(dms.Model):
             N=self.timepoint_counts_total,
             shape=(self.n_replicates, self.n_timepoints - 1, self.n_variants),
         )
+
+    @abstractmethod
+    def _define_non_base_parameters(self, **hyperparameters):
+        """
+        Define any non-base parameters for the model. This should be implemented
+        in subclasses to define any additional parameters needed for the model.
+        """
 
     @abstractmethod
     def _define_growth_function(self) -> dms_components.TransformedParameter:
@@ -189,11 +181,69 @@ class TrpBBaseGrowthModel(dms.Model):
         return self._n_variants
 
 
-class TrpBExponentialGrowth(TrpBBaseGrowthModel):
+class ExponentialMixIn:
+    """Mix-in class for models that use an exponential growth function."""
+
+    def _define_growth_function(self) -> dms_components.ExponentialGrowth:
+        """
+        Defines the growth function for the model. This should return a
+        TransformedParameter object that defines the growth function.
+        """
+        # pylint: disable=no-member
+        return dms_components.ExponentialGrowth(
+            t=self.tg0,
+            A=self.theta_t0,
+            r=self.r,
+            shape=(self.n_replicates, self.n_timepoints - 1, self.n_variants),
+        )
+
+
+class SigmoidMixIn:
+    """Mix-in class for models that use a sigmoid growth function."""
+
+    def __init__(
+        self,
+        times: npt.NDArray[np.floating],
+        starting_counts: npt.NDArray[np.integer],
+        timepoint_counts: npt.NDArray[np.integer],
+        alpha: float = 0.75,
+        c_alpha: float = 4.0,
+        c_beta: float = 8.0,
+        **hyperparameters,
+    ):
+        # Define the inflection point of the sigmoid function
+        self.c = dms_components.Gamma(alpha=c_alpha, beta=c_beta)
+
+        # Run inherited init
+        super().__init__(
+            times=times,
+            starting_counts=starting_counts,
+            timepoint_counts=timepoint_counts,
+            alpha=alpha,
+            **hyperparameters,
+        )
+
+    def _define_growth_function(
+        self,
+    ) -> dms_components.SigmoidGrowthInitParametrization:
+        """
+        Defines the growth function for the model. This should return a
+        TransformedParameter object that defines the growth function.
+        """
+        # pylint: disable=no-member
+        return dms_components.SigmoidGrowthInitParametrization(
+            t=self.tg0,
+            x0=self.theta_t0,
+            r=self.r,
+            c=self.c,
+            shape=(self.n_replicates, self.n_timepoints - 1, self.n_variants),
+        )
+
+
+class BaseGammaInvRate(BaseGrowthModel):
     """
-    Models the TrpB count data from Johnston et al. using an exponential growth
-    function to model the time-dependent increase in counts and a multinomial distribution
-    to model the counts at each timepoint.
+    Base model for all TrpB growth models that parametrize the inverse of the mean
+    growth rate as gamma distributed and the individual growth rates as exponential.
     """
 
     def __init__(
@@ -215,26 +265,31 @@ class TrpBExponentialGrowth(TrpBBaseGrowthModel):
             inv_r_beta=inv_r_beta,
         )
 
-    def _define_growth_function(self) -> dms_components.ExponentialGrowth:
-        """
-        Defines the growth function for the model. This should return a
-        TransformedParameter object that defines the growth function.
-        """
-        return dms_components.ExponentialGrowth(
-            t=self.tg0,
-            A=self.theta_t0,
-            r=self.r,
-            shape=(self.n_replicates, self.n_timepoints - 1, self.n_variants),
+    def _define_non_base_parameters(  # pylint: disable=arguments-differ
+        self, inv_r_alpha: float, inv_r_beta: float
+    ):
+        # Every variant has a fundamental "rate" which will be the same across
+        # experiments. This rate is modeled by the Gamma distribution and is the
+        # inverse of the growth rate (so that we can use it as the exponential rate
+        # parameter).
+        self.inv_r_mean = dms_components.Gamma(
+            alpha=inv_r_alpha,
+            beta=inv_r_beta,
+            shape=(self.n_variants,),
+        )
+
+        # The inverse rate is the beta parameter for the exponential distributions
+        # describing the growth rates in each experiment.
+        self.r = dms_components.Exponential(
+            beta=self.inv_r_mean,
+            shape=(self.n_replicates, 1, self.n_variants),
         )
 
 
-class TrpBSigmoidGrowth(TrpBBaseGrowthModel):
+class BaseFoldChangeRate(BaseGrowthModel):
     """
-    Models the TrpB count data from Johnston et al. using a sigmoid growth
-    function to model the time-dependent increase in counts and a multinomial distribution
-    to model the counts at each timepoint. This model uses the initial proportions
-    as a parameter, which is then used to calculate the proportions at t > 0. The
-    initial proportions are assumed to capture the `c` parameter implicitly.
+    Base model for all TrpB growth models that parametrize the growth rate as a
+    as a fold-change of some mean growth rate.
     """
 
     def __init__(
@@ -243,41 +298,156 @@ class TrpBSigmoidGrowth(TrpBBaseGrowthModel):
         starting_counts: npt.NDArray[np.integer],
         timepoint_counts: npt.NDArray[np.integer],
         alpha: float = 0.75,
-        inv_r_alpha: float = 2.5,
-        inv_r_beta: float = 0.5,
-        c_alpha: float = 4.0,
-        c_beta: float = 8.0,
+        log_foldchange_sigma_sigma: float = 0.1,
+        **hyperparameters,
     ):
-        # We have an additional parameter for the sigmoid growth model, `c`, which
-        # defines the time at which the growth rate is half of its maximum value.
-        # We know that this has to be greater than 0, but is unlikely to be close
-        # to 0. Also, we're going to assume that differences in growth conditions
-        # could allow for differences in the time at which the maximum growth rate
-        # is reached, so we will model different values of `c` for each replicate
-        # using the Gamma distribution.
-        self.c = dms_components.Gamma(
-            alpha=c_alpha,
-            beta=c_beta,
-            shape=(timepoint_counts.shape[0], 1, 1),
-        )
-
         # Run inherited init
         super().__init__(
             times=times,
             starting_counts=starting_counts,
             timepoint_counts=timepoint_counts,
             alpha=alpha,
-            inv_r_alpha=inv_r_alpha,
-            inv_r_beta=inv_r_beta,
+            log_foldchange_sigma_sigma=log_foldchange_sigma_sigma,
+            **hyperparameters,
         )
 
-    def _define_growth_function(
-        self,
-    ) -> dms_components.SigmoidGrowthInitParametrization:
-        return dms_components.SigmoidGrowthInitParametrization(
-            t=self.tg0,
-            x0=self.theta_t0,
-            r=self.r,
-            c=self.c,
-            shape=(self.n_replicates, self.n_timepoints - 1, self.n_variants),
+    @abstractmethod
+    def _define_growth_distribution(self, **hyperparameters):
+        """
+        Define the growth rate distribution for the model. This should return a
+        Parameter object.
+        """
+
+    def _define_non_base_parameters(  # pylint: disable=arguments-differ
+        self, log_foldchange_sigma_sigma: float, **hyperparameters
+    ):
+
+        # Define the typical growth rate
+        self.r_raw = self._define_growth_distribution(**hyperparameters)
+
+        # The error on the log of the fold-change is modeled as a half-normal distribution.
+        # We assume homoscedasticity in fold-change error
+        self.log_foldchange_sigma = dms_components.HalfNormal(
+            sigma=log_foldchange_sigma_sigma
         )
+
+        # The fold-change is modeled as a log-normal distribution (i.e., the log
+        # of the fold-change is normally distributed) with a mean of 0 (i.e., the
+        # typical fold-change is 1).
+        self.fold_change = dms_components.LogNormal(
+            mu=dms_components.Constant(0.0, togglable=False),
+            sigma=self.log_foldchange_sigma,
+            shape=(self.n_replicates, 1, self.n_variants),
+        )
+
+        # The rate is then the product of the mean rate and the fold-change
+        self.r = self.r_raw * self.fold_change
+
+
+class BaseExponentialRate(BaseFoldChangeRate):
+    """
+    Base model for all TrpB growth models that parametrize the mean growth rate
+    as exponential distributed and the individual growth rates as being derived
+    from some fold-change of the mean.
+    """
+
+    def __init__(
+        self,
+        times: npt.NDArray[np.floating],
+        starting_counts: npt.NDArray[np.integer],
+        timepoint_counts: npt.NDArray[np.integer],
+        alpha: float = 0.75,
+        log_foldchange_sigma_sigma: float = 0.1,
+        beta: float = 1.0,
+    ):
+        # Run inherited init
+        super().__init__(
+            times=times,
+            starting_counts=starting_counts,
+            timepoint_counts=timepoint_counts,
+            alpha=alpha,
+            log_foldchange_sigma_sigma=log_foldchange_sigma_sigma,
+            beta=beta,
+        )
+
+    def _define_growth_distribution(  # pylint: disable=arguments-differ
+        self, beta: float
+    ):
+        # The mean growth rate is modeled as an exponential distribution
+        return dms_components.Exponential(beta=beta, shape=(self.n_variants,))
+
+
+class BaseLomaxRate(BaseFoldChangeRate):
+    """
+    Base model for all TrpB growth models that parametrize the mean growth rate
+    as Lomax (Paretto Type II) distributed and the individual growth rates as being
+    derived from some fold-change of the mean. This is similar to the exponential
+    model, but allows for a heavier tail.
+    """
+
+    def __init__(
+        self,
+        times: npt.NDArray[np.floating],
+        starting_counts: npt.NDArray[np.integer],
+        timepoint_counts: npt.NDArray[np.integer],
+        alpha: float = 0.75,
+        log_foldchange_sigma_sigma: float = 0.1,
+        lambda_: float = 1.0,
+        lomax_alpha: float = 2.5,
+    ):
+        # Run inherited init
+        super().__init__(
+            times=times,
+            starting_counts=starting_counts,
+            timepoint_counts=timepoint_counts,
+            alpha=alpha,
+            log_foldchange_sigma_sigma=log_foldchange_sigma_sigma,
+            lambda_=lambda_,
+            lomax_alpha=lomax_alpha,
+        )
+
+    def _define_growth_distribution(  # pylint: disable=arguments-differ
+        self, lambda_: float, lomax_alpha: float
+    ):
+        return dms_components.Lomax(
+            lambda_=lambda_, alpha=lomax_alpha, shape=(self.n_variants,)
+        )
+
+
+class GammaInvRateExponGrowth(ExponentialMixIn, BaseGammaInvRate):
+    """
+    Models the TrpB count data from Johnston et al. using an exponential growth
+    function to model the time-dependent increase in counts and a multinomial distribution
+    to model the counts at each timepoint. The prior on the inverse growth rate is
+    gamma distributed.
+    """
+
+
+class GammaInvRateSigmoidGrowth(SigmoidMixIn, BaseGammaInvRate):
+    """Gamma-distributed inverse rate parameter with a sigmoid growth function."""
+
+
+class ExponRateExponGrowth(ExponentialMixIn, BaseExponentialRate):
+    """
+    Models the TrpB count data from Johnston et al. using an exponential growth
+    function to model the time-dependent increase in counts and a multinomial distribution
+    to model the counts at each timepoint. The prior on the growth rate is
+    exponential.
+    """
+
+
+class ExponRateSigmoidGrowth(SigmoidMixIn, BaseExponentialRate):
+    """Exponential-distributed rate parameter with a sigmoid growth function."""
+
+
+class LomaxRateExponGrowth(ExponentialMixIn, BaseLomaxRate):
+    """
+    Models the TrpB count data from Johnston et al. using an exponential growth
+    function to model the time-dependent increase in counts and a multinomial distribution
+    to model the counts at each timepoint. The prior on the growth rate is
+    Lomax-distributed.
+    """
+
+
+class LomaxRateSigmoidGrowth(SigmoidMixIn, BaseLomaxRate):
+    """Lomax-distributed rate parameter with a sigmoid growth function."""
