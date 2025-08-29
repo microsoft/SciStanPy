199a30c26929a7a60042fbd1f0f59317ef3affb7
Bruce Wittmann <bwittmann@microsoft.com>
2025-08-29 16:09:00 -0700

    Fixed import path

--- scistanpy/model/model.py
+++ scistanpy/model/model.py
@@ -61,7 +61,7 @@ if TYPE_CHECKING:
     from scistanpy import custom_types
     from scistanpy.model.results import hmc as hmc_results
 
-mle_module = utils.lazy_import("scistanpy.model.mle")
+mle_module = utils.lazy_import("scistanpy.model.results.mle")
 nn_module = utils.lazy_import("scistanpy.model.nn_module")
 prior_predictive_module = utils.lazy_import("scistanpy.plotting.prior_predictive")
 stan_model = utils.lazy_import("scistanpy.model.stan.stan_model")
